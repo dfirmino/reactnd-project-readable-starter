d20997cb9864566252a1c2429366a3ae6d5db785
Davi Firmino <davi.firmino@smartnx.com>
2018-07-10 09:43:20 -0300

    removendo arquivos

--- frontend/readable/src/index.js
+++ frontend/readable/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers/index'
-import './index.css';
 import App from './components/App';
 
 const store = createStore(
